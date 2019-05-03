56e568f7fcb48a2abec9db0d41d99e69dc8e2cca
hiroyuki ichikawa <yaritsusozai@gmail.com>
2019-05-03 11:25:53 +0900

    LINE用（選択肢、画像）取り込み追加

--- 3.裾野方式LINEBotスプレトシート入力/conv_csv_json.py
+++ 3.裾野方式LINEBotスプレトシート入力/conv_csv_json.py
@@ -1,4 +1,8 @@
 # coding:utf-8
+# 市川博之（Hiroyuki Ichikawa） 2019/5/3
+# var 1.10 LINE用の基本コマンド対応
+#
+#
 import csv
 import random
 import sys
@@ -42,14 +46,80 @@ with open(filename, 'r') as f:
 		out_f.write(str)
 		str = "\t\t\"messages\": [,\n"
 		out_f.write(str)
-		str = "\t\t\t{\n"
+		str = "\t\t\t{\n"			# 通常メッセージの始まり
 		out_f.write(str)
 		str = "\t\t\t\"type\" : 0, \"lang\" : \"ja\", \"speech\" : \"" + row[2] + "\"\n"
 		out_f.write(str)
 		str = "\t\t\t},{\n"
 		out_f.write(str)
 		str = "\t\t\t\"type\" : 0, \"lang\" : \"ja\", \"speech\" : []\n"
-		out_f.write(str)
+		out_f.write(str)			# LINE用に追加するならこの行の次に入れる
+		if len(row[6])>0:			# LINE用の文字列
+			str = "\t\t\t},{\n"
+			out_f.write(str)
+			str = "\t\t\t\"type\" : 0, \"platform\" : \"line\", \"lang\" : \"ja\", \"speech\" : \"" + row[6] + "\"\n"
+			out_f.write(str)
+		if len(row[7])>0:			# LINE用の画像
+			str = "\t\t\t},{\n"
+			out_f.write(str)
+			str = "\t\t\t\"type\" : 3, \"platform\" : \"line\", \"lang\" : \"ja\", \"imageUrl\" : \"" + row[7] + "\"\n"
+			out_f.write(str)
+		if len(row[8])>0:			# LINE用のボタン型選択肢
+			str = "\t\t\t},{\n"
+			out_f.write(str)
+			str = "\t\t\t\"type\" : 2, \"platform\" : \"line\", \"lang\" : \"ja\", \"title\" : \"" + row[8] + "\", \"replies\" : ["
+			out_f.write(str)
+			if len(row[9])>0:			# LINE用のボタン型選択肢
+				str = "\"" + row[9] + "\""
+				out_f.write(str)
+			if len(row[10])>0:			# LINE用のボタン型選択肢
+				str = ",\"" + row[10] + "\""
+				out_f.write(str)
+			if len(row[11])>0:			# LINE用のボタン型選択肢
+				str = ",\"" + row[11] + "\""
+				out_f.write(str)
+			str = "]\n"
+			out_f.write(str)
+		if len(row[12])>0:			# LINE用の画像付きボタン
+			str = "\t\t\t},{\n"
+			out_f.write(str)
+			str = "\t\t\t\"type\" : 1, \"platform\" : \"line\", \"lang\" : \"ja\", \"title\" : \"" + row[12] + "\" , \"subtitle\" : \"" + row[13] + "\" , \"imageUrl\" : \"" + row[14] + "\" , \"buttons\" : ["
+			out_f.write(str)
+			if len(row[15])>0:			# LINE用のボタン型選択肢
+				str = "{\"text\" : " + "\"" + row[15] + "\""
+				out_f.write(str)
+				if len(row[16])>0:			# LINE用のボタン型選択肢
+					str = ",\"postback\" : " + "\"" + row[16] + "\""
+					out_f.write(str)
+				str = "}"
+				out_f.write(str)
+			if len(row[17])>0:				# LINE用のボタン型選択肢
+				str = ",{\"text\" : " + "\"" + row[17] + "\""
+				out_f.write(str)
+				if len(row[18])>0:			# LINE用のボタン型選択肢
+					str = ",\"postback\" : " + "\"" + row[18] + "\""
+					out_f.write(str)
+				str = "}"
+				out_f.write(str)
+			if len(row[19])>0:				# LINE用のボタン型選択肢
+				str = ",{\"text\" : " + "\"" + row[19] + "\""
+				out_f.write(str)
+				if len(row[20])>0:			# LINE用のボタン型選択肢
+					str = ",\"postback\" : " + "\"" + row[20] + "\""
+					out_f.write(str)
+				str = "}"
+				out_f.write(str)
+
+			str = "]\n"
+			out_f.write(str)
+
+		if len(row[21])>0:			# LINE用のCustom
+			str = "\t\t\t},{\n"
+			out_f.write(str)
+			str = "\t\t\t\"type\" : 4, \"platform\" : \"line\", \"lang\" : \"ja\", \"payload\" : { \"line\" : " + row[21] + " }"
+			out_f.write(str)
+
+		# LINE用終わり
 		str = "\t\t\t}\n\t\t],\n"
 		out_f.write(str)
 		str = "\t\t\"defaultResponsePlatforms\" : {},\n"
